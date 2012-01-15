988347e11b0632f4b280fe895531f76beb418d23
Jaroslav Kostal <jako512@gmail.com>
2012-01-15 18:45:13 +0100

    get rid of the appName in URL

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -10,6 +10,8 @@
 //    grails.config.locations << "file:" + System.properties["${appName}.config.location"]
 // }
 
+// get rid of the appName in URL
+grails.app.context = '/'
 
 grails.project.groupId = appName // change this to alter the default package name and Maven publishing destination
 grails.mime.file.extensions = true // enables the parsing of file extensions from URLs into the request format
